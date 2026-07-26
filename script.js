dd36fee0794f3cdc27ba191cbc6564fef7ed8f85
amatushek <amatushek@gmail.com>
2026-07-26 18:52:54 -0500

    another fix to script.js

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-const raceDate = new Date("September 20, 2026");
+function showWeek(week) {
 
-function countdown(){
+    const container = document.getElementById("workoutContainer");
 
-const today = new Date();
-
-const difference = raceDate - today;
-
-const days = Math.ceil(
-difference / (1000*60*60*24)
-);
-
-document.getElementById("countdown").innerHTML =
-days + " days until race day";
+    if (workouts[week]) {
+        container.innerHTML = workouts[week];
+    } else {
+        container.innerHTML = "<h2>Workout not found</h2>";
+    }
 
 }
 
-countdown();
+
+const raceDate = new Date("September 20, 2026");
+
+
+function updateCountdown() {
+
+    const today = new Date();
+
+    const difference = raceDate - today;
+
+    const days = Math.ceil(
+        difference / (1000 * 60 * 60 * 24)
+    );
+
+    document.getElementById("countdown").innerHTML =
+        "<h2>" + days + " Days Until Race Day</h2>";
+
+}
+
+
+updateCountdown();
+
+showWeek(1);
